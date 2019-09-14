5c439d51536cc5deaccdf42a8bdf32fce3bcf8aa
Surendarmurugesan <53648189+Surendarmurugesan@users.noreply.github.com>
2019-09-14 11:25:00 +0530

    Update freestyleJob.groovy

--- freestyleJob.groovy
+++ freestyleJob.groovy
@@ -1,6 +1,6 @@
 freeStyleJob('Freestyle Script'){
 	description('My Freestyle Script.')
 	 scm {
-        github('https://github.com/Surendarmurugesan/GITSession.git')
+        github('Surendarmurugesan/GITSession.git/')
     }
 }
