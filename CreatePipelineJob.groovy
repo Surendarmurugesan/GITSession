9ce2291f35c856c2587cfbba6ecab77c7efbf5d0
Surendarmurugesan <53648189+Surendarmurugesan@users.noreply.github.com>
2019-09-29 10:07:59 +0530

    Update CreatePipelineJob.groovy

--- CreatePipelineJob.groovy
+++ CreatePipelineJob.groovy
@@ -1,9 +1,7 @@
-pipelineJob('Pipeline-2')
+pipelineJob('Pipeline Demo')
 {
   definition{
-    cpsScm{
       scm {
-        github('Surendarmurugesan/GITSession', 'master')
         scriptPath('CI.groovy')
         def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
         approvals.approveScript(approvals.hash(scriptPath,"groovy"))
