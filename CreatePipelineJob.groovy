f9702dbaf9c0c6988f4943e7c0ed5c3188d2a33b
Surendarmurugesan <53648189+Surendarmurugesan@users.noreply.github.com>
2019-09-29 10:22:28 +0530

    Update CreatePipelineJob.groovy

--- CreatePipelineJob.groovy
+++ CreatePipelineJob.groovy
@@ -2,9 +2,7 @@ pipelineJob('Pipeline Demo')
 {
   definition{
       cps{
-       script(readFileFromWorkspace('CI.groovy'))
-      def approvals = org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval.get()
-      approvals.approveScript(approvals.hash(script,"groovy")))
+       script(readFileFromWorkspace('CI.groovy')
     }
   }
 }
